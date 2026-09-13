ff110cea02a6cc7a7d4b90bfe0bbcf1d4c3145ce
JonasG <jonas.grgt@gmail.com>
2026-09-13 11:34:20 +0200

    feat(server): extract ClusterConnections

---
+++ kawa-server/src/test/java/io/jonasg/kawa/server/broker/ClusterConnectionsTest.java
@@ -0,0 +1,34 @@
+package io.jonasg.kawa.server.broker;
+
+import io.jonasg.kawa.core.InterceptorPipeline;
+import io.jonasg.kawa.core.cluster.MetadataCache;
+import io.jonasg.kawa.core.metrics.GatewayMetrics;
+import io.jonasg.kawa.protocol.kafka.KafkaApiRegistry;
+import io.jonasg.kawa.protocol.kafka.KafkaBodyCodec;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ClusterConnectionsTest {
+
+    @Test
+    void closeIsSafeWithoutStart() {
+        // given
+        var metrics = new GatewayMetrics(new SimpleMeterRegistry());
+        var codec = new KafkaBodyCodec(KafkaApiRegistry.create());
+        var pipeline = new InterceptorPipeline(List.of());
+        var cache = new MetadataCache();
+        var connections = new ClusterConnections(codec, pipeline, metrics, cache, "localhost", 9092, null);
+
+        // when
+        connections.close();
+
+        // then
+        assertThat(connections.pool()).isNotNull();
+        assertThat(connections.metadataClient()).isNotNull();
+        assertThat(connections.router()).isNotNull();
+    }
+}
